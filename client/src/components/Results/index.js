f0a6754ff7d94574f20254aa0bb7259d8c4e317b
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-19 15:51:45 +0100

    Adding Results component

---
+++ client/src/components/Results/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BookContainer from "../BookContainer";
+import Container from "../Container";
+
+function Results() {
+    return (
+        <Container>
+            <h3>Results</h3>
+            <BookContainer />
+        </Container>
+    );
+}
+
+export default Results;
