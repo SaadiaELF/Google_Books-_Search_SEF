8d2af81cb38fc17392d4a7ef87c7b54e5d5ef5d4
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-19 13:50:40 +0100

    Adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "../src/pages/Search"
 
+
+// Calling all components and using router to render pages
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
-}
+    <Router>
+      <div>
 
+        {/* <Route exact path="/" component={AboutMe} /> */}
+        <Route exact path="/search" component={Search} />
+      </div>
+    </Router>
+  )
+
+}
 
 export default App;
