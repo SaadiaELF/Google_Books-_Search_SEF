297577bb762b6dbab8b9718d8b32faadaba5c279
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-19 15:50:51 +0100

    Adding bookContainer compom=ent

---
+++ client/src/components/BookContainer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BookContainer(props) {
+    return (
+        <div className="container my-5">
+            <div className="px-4 py-5 my-5 text-center align-items-center rounded-3 border shadow-lg">
+            {props.children}
+            </div>
+        </div>
+    );
+}
+
+export default BookContainer;
