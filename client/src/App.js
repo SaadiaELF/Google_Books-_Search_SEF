689ba01de89aa2aba51f45880538025cd256e85a
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-19 15:16:54 +0100

    Creating saved page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "../src/pages/Search"
-
+import Saved from "../src/pages/Saved"
 
 // Calling all components and using router to render pages
 function App() {
@@ -11,6 +11,7 @@ function App() {
 
         <Route exact path="/" component={Search} />
         <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
       </div>
     </Router>
   )
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+function Saved() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+};
+
+
+export default Saved;
