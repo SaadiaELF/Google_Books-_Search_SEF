3597f007176610bfa16ccd496da50749d1f3d819
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-19 10:57:22 +0100

    Adding book model

---
+++ models/book.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    authors: [String],
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+});
+
+const Book = mongoose.model("Book", bookSchema;
+
+module.exports = Book;
