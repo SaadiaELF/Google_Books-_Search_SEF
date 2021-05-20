c77d3f2e49a1dae13c9a06fb54803149b7b0a7c0
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-20 19:59:19 +0100

    Refactoring the component

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,12 +1,42 @@
 import React from "react";
-import BookContainer from "../BookContainer";
+import BookDescription from "../BookDescription";
+import BookHeader from "../BookHeader";
+import BtnGroup from "../BtnGroup";
+import Button from "../Button";
+import Col from "../Col";
 import Container from "../Container";
+import Image from "../Image";
+import Row from "../Row";
 
-function Results() {
+function Results(props) {
     return (
         <Container>
             <h3>Results</h3>
-            <BookContainer />
+            <Container>
+            <Row padding="" border="">
+                <Col size="md-10">
+                    <BookHeader>
+                        title ={props.title}
+                        subtitle ={props.subtitle}
+                        authors ={props.authors}
+                    </BookHeader>
+                </Col>
+                <Col size="md-2">
+                    <BtnGroup>
+                        <Button type="secondary">View</Button>
+                        <Button type="danger">Save</Button>
+                    </BtnGroup>
+                </Col>
+            </Row>
+            <Row padding="p-2" border="border">
+                <Col size="md-2">
+                    <Image image={props.image} title={props.title} />
+                </Col>
+                <Col size="md-10">
+                    <BookDescription description={props.description} />
+                </Col>
+            </Row>
+        </Container >
         </Container>
     );
 }
