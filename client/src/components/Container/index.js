ff5bddfd0e29096717e3974547869329ea98162e
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-20 22:23:17 +0100

    Updating margin

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Container(props) {
     return (
-        <div className="container my-5">
-            <div className="px-4 py-5 my-5 text-center align-items-center rounded-3 border shadow-lg">
+        <div className="container my-2">
+            <div className="px-4 py-5 my-2 text-center align-items-center rounded-3 border shadow-lg">
             {props.children}
             </div>
         </div>
