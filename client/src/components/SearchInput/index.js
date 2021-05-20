532085d4fef537c69a5d20490c383c04485ea17d
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-20 22:24:20 +0100

    Adding props

--- client/src/components/SearchInput/index.js
+++ client/src/components/SearchInput/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import Container from "../Container";
 
-function SearchInput() {
+function SearchInput(props) {
     return (
         <Container>
-            <h3>Book Search</h3>
-            <p className="lead mb-4">Enter below the title of the book you want to look for : </p>
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Book's title" aria-label="Book's title" aria-describedby="button-addon" />
-                    <button className="btn btn-outline-secondary" type="button" id="button-addon">Search</button>
+            <form className="search">
+                <h3>Book Search</h3>
+                <p className="lead mb-4">Enter below the title of the book you want to look for : </p>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <div className="input-group mb-3">
+                        <input
+                            value={props.search}
+                            onChange={props.handleInputChange}
+                            name="term"
+                            list="term"
+                            id="term"
+                            type="text"
+                            className="form-control"
+                            placeholder="Book's title"
+                            aria-label="Book's title"
+                            aria-describedby="button-addon"
+                        />
+                        <button className="btn btn-outline-secondary" type="button" id="button-addon">Search</button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </Container>
     );
 }
