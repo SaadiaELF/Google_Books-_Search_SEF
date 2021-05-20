3fce287147300c564cb4b8d3b49014fac98e6933
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-20 18:18:50 +0100

    Updating db connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const routes = require("./routes")
+// const routes = require("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,9 +13,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
