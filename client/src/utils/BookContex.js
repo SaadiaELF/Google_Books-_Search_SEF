254fee6f2c988927759eeb591ecd985daed87d41
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-20 21:11:18 +0100

    Creating BookContext

---
+++ client/src/utils/BookContex.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookContext = React.createContext({
+    title: "",
+    subtitle: "",
+    authors: "",
+    image: "",
+    description: ""
+});
+
+export default BookContext;
