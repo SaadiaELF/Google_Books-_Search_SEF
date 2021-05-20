ba7d2ac7969ad27ce210ee0fe97e2fd8be6f7159
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-20 20:06:04 +0100

    Adding favorites component

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Favorites from "../components/Favorites";
 function Saved() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Favorites />
         </div>
     )
 };
