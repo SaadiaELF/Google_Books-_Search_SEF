92ea12af7e3b9682a5b011732829868aad86841a
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-21 01:37:36 +0100

    Updating api call

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,43 +3,23 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Results from "../components/Results";
 import SearchInput from "../components/SearchInput";
-import Alert from "../components/Alert"
 import API from "../utils/API";
 import BookContext from "../utils/BookContext"
 
 function Search() {
 
-    const [book, setBook] = useState({
-        title: "",
-        subtitle: "",
-        authors: "",
-        image: "",
-        description: ""
-    });
+    const [books, setBooks] = useState([]);
     const [search, setSearch] = useState("");
-    const [error, setError] = useState("");
 
     useEffect(() => {
         if (!search) {
             return;
         }
         API.search(search)
-            .then(res => {
-                if (res.data.length === 0) {
-                    throw new Error("No results found.");
-                }
-                if (res.data.status === "error") {
-                    throw new Error(res.data.message);
-                }
-                setBook({
-                    title: res.data.items[0].volumeInfo.title,
-                    subtitle: res.data.items[0].volumeInfo.subtitle,
-                    authors: res.data.items[0].volumeInfo.authors,
-                    image: res.data.items[0].volumeInfo.imageLinks.smallThumbnail,
-                    description: res.data.items[0].volumeInfo.description
-                });
+            .then(books => {
+                setBooks(books);
             })
-            .catch(err => setError(err));
+            .catch(err => console.log(err));
     }, [search]);
 
     const handleInputChange = event => {
@@ -51,13 +31,10 @@ function Search() {
     };
 
     return (
-        <BookContext.Provider value={book}>
+        <BookContext.Provider value={[books]}>
             <div>
                 <Navbar />
                 <Hero />
-                <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 5 }}>
-                    {error}
-                </Alert>
                 <SearchInput
                     handleFormSubmit={handleFormSubmit}
                     handleInputChange={handleInputChange}
