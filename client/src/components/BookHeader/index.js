ed3e314acfa3c3d2affcb27aea079169ca0cf12c
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-21 23:27:58 +0100

    Updating title fields

--- client/src/components/BookHeader/index.js
+++ client/src/components/BookHeader/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function BookHeader({ title, subtitle, authors }) {
     return (
         <div>
-            <h4 className="text-start"> Title : {title}</h4>
-            <h6 className="text-start"> Subtitle :  {subtitle}</h6>
+            <h4 className="text-start"> {title}</h4>
+            <h6 className="text-start"> {subtitle}</h6>
             <p className="text-start">Written by : {authors} </p>
         </div>
     );
